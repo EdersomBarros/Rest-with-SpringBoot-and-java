01b65e0ee08cd2831372e89701af79db1c34264f
ederson barros <edersombarros@hotmail.com>
2024-06-06 14:10:35 -0400

    first in java with spring boot

---
+++ Rest-with-SpringBoot-and-java/src/main/java/br/com/Rest_with/_SpringBoot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.Rest_with._SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
