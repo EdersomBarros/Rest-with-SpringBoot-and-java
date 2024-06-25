0b7670fdd4887bfe914b6beb69730d4fa110e037
ederson barros <edersombarros@hotmail.com>
2024-06-25 15:15:29 -0400

    configuration Cross-Origin

--- Rest-with-SpringBoot-and-java/src/test/java/br/com/Rest_with/_SpringBoot/configs/TestConfigs.java
+++ Rest-with-SpringBoot-and-java/src/test/java/br/com/Rest_with/_SpringBoot/configs/TestConfigs.java
@@ -3,5 +3,13 @@ package br.com.Rest_with._SpringBoot.configs;
 public class TestConfigs {
 
 	public static final int SERVER_PORT = 8888;
+	public static final String HEADER_PARAM_ORIGIN = "Origin";
+	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+	public static final String CONTENT_TYPE_JSON = "application/json";
+	public static final String XML = "application/xml";
+	public static final String CONTENT_TYPE_YML = "application/x-yaml";
+	
+	public static final String ORIGIN_GITHUB = "https://edersombarros.github.io";
+	public static final String ORIGIN_SEMERU = "https://semeru.github.io";
 
 }
